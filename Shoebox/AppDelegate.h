c18a187d656e928765269adb0d2c8f7e71a29dd9
Steve Derico <steve@stevederico.com>
2012-06-03 20:54:44 -0700

    First Commit

---
+++ Shoebox/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Shoebox
+//
+//  Created by Stephen Derico on 6/3/12.
+//  Copyright (c) 2012 Bixby Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

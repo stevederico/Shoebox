8975f8230b75be18fe83f7915a74d7b570d5f1b2
Steve Derico <steve@stevederico.com>
2012-06-03 21:04:31 -0700

    Added CoreData

---
+++ Shoebox/CreateGroupViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateGroupViewController.h
+//  Shoebox
+//
+//  Created by Stephen Derico on 6/3/12.
+//  Copyright (c) 2012 Bixby Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateGroupViewController : UITableViewController
+
+@end
---
+++ Shoebox/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  Shoebox
+//
+//  Created by Stephen Derico on 6/3/12.
+//  Copyright (c) 2012 Bixby Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UITableViewController
+
+@end
